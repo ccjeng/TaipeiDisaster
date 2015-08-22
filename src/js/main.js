d8b12e72cfb780dce4171e9d5dfbf62ce8ecdb97
Andy Cheng <ccjeng@gmail.com>
2015-08-22 09:51:00 +0800

    更新資料格式

--- src/js/main.js
+++ src/js/main.js
@@ -24,17 +24,17 @@ app.controller('GetDisasterSummary', ['$scope', '$http', function($scope, $http)
       
         var marker = new google.maps.Marker({
             map: $scope.map,
-            position: new google.maps.LatLng(info.Wgs84Y[0], info.Wgs84X[0]),
-            title: info.PName[0] + ' - '+ info.Name[0],
+            position: new google.maps.LatLng(info.Wgs84Y, info.Wgs84X),
+            title: info.PName + ' - '+ info.Name,
         });
 
 
-        marker.content = '<div class="infoWindowContent">發生時間：' + (isEmpty(d[i].CaseTime) ? '' : info.CaseTime[0])
-          + '</br>災情地點：' + info.CaseLocationDescription[0]
-          + '</br>災情描述：' + info.CaseDescription[0] 
+        marker.content = '<div class="infoWindowContent">發生時間：' + (isEmpty(d[i].CaseTime) ? '' : info.CaseTime)
+          + '</br>災情地點：' + info.CaseLocationDescription
+          + '</br>災情描述：' + info.CaseDescription 
           + '</div>';
         
-        if (info.CaseComplete[0]==='true') {
+        if (info.CaseComplete==='true') {
           marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
         }
 
@@ -86,84 +86,87 @@ app.controller('GetDisasterSummary', ['$scope', '$http', function($scope, $http)
     .success(function(data, status, headers, config) {
       //console.log('call json');
 
-      d = data.DataSet['diffgr:diffgram'][0].NewDataSet[0]['CASE_SUMMARY'];
-      $scope.cases = d; //data.DataSet['diffgr:diffgram'][0].NewDataSet[0]['CASE_SUMMARY'];
+      //d = data.DataSet['diffgr:diffgram'].NewDataSet['CASE_SUMMARY'];
+      d = data.DataSet['diffgr:diffgram'].NewDataSet['CASE_SUMMARY'];
+      //console.log(d);
+
+      $scope.cases = d;
 
       var caseDone = 0, caseInProgress = 0, caseRate = 0.00;
       for (i = 0; i < d.length; i++){
         createMarker(d[i]);
       
-        if (d[i].CaseComplete[0]==='true') {
+        if (d[i].CaseComplete==='true') {
             caseDone = caseDone + 1;
   
-            if (d[i].PName[0]==='路樹災情') {
+            if (d[i].PName==='路樹災情') {
               $scope.doneTotal['路樹災情'] += 1;
             }
-            if (d[i].PName[0]==='民生、基礎設施災情') {
+            if (d[i].PName==='民生、基礎設施災情') {
               $scope.doneTotal['民生、基礎設施災情'] += 1;
             }
-            if (d[i].PName[0]==='建物毀損') {
+            if (d[i].PName==='建物毀損') {
               $scope.doneTotal['建物毀損'] += 1;
             }
-            if (d[i].PName[0]==='廣告招牌災情') {
+            if (d[i].PName==='廣告招牌災情') {
               $scope.doneTotal['廣告招牌災情'] += 1;
             }
-            if (d[i].PName[0]==='積淹水災情') {
+            if (d[i].PName==='積淹水災情') {
               $scope.doneTotal['積淹水災情'] += 1;
             }
-            if (d[i].PName[0]==='其他災情') {
+            if (d[i].PName==='其他災情') {
               $scope.doneTotal['其他災情'] += 1;
             }
-            if (d[i].PName[0]==='土石災情') {
+            if (d[i].PName==='土石災情') {
               $scope.doneTotal['土石災情'] += 1;
             }
-            if (d[i].PName[0]==='道路、隧道災情') {
+            if (d[i].PName==='道路、隧道災情') {
               $scope.doneTotal['道路、隧道災情'] += 1;
             }
-            if (d[i].PName[0]==='水利設施災害') {
+            if (d[i].PName==='水利設施災害') {
               $scope.doneTotal['水利設施災情'] += 1;
             }
-            if (d[i].PName[0]==='鐵路、高鐵及捷運災情') {
+            if (d[i].PName==='鐵路、高鐵及捷運災情') {
               $scope.doneTotal['鐵路、高鐵及捷運災情'] += 1;
             }
-            if (d[i].PName[0]==='車輛及交通事故') {
+            if (d[i].PName==='車輛及交通事故') {
               $scope.doneTotal['車輛及交通事故'] += 1;
             }
         }else {
             caseInProgress = caseInProgress + 1;
         }
 
-        if (d[i].PName[0]==='路樹災情') {
+        if (d[i].PName==='路樹災情') {
           $scope.total['路樹災情'] += 1;
         }
-        if (d[i].PName[0]==='民生、基礎設施災情') {
+        if (d[i].PName==='民生、基礎設施災情') {
           $scope.total['民生、基礎設施災情'] += 1;
         }
-        if (d[i].PName[0]==='建物毀損') {
+        if (d[i].PName==='建物毀損') {
           $scope.total['建物毀損'] += 1;
         }
-        if (d[i].PName[0]==='廣告招牌災情') {
+        if (d[i].PName==='廣告招牌災情') {
           $scope.total['廣告招牌災情'] += 1;
         }
-        if (d[i].PName[0]==='積淹水災情') {
+        if (d[i].PName==='積淹水災情') {
           $scope.total['積淹水災情'] += 1;
         }
-        if (d[i].PName[0]==='其他災情') {
+        if (d[i].PName==='其他災情') {
           $scope.total['其他災情'] += 1;
         }
-        if (d[i].PName[0]==='土石災情') {
+        if (d[i].PName==='土石災情') {
           $scope.total['土石災情'] += 1;
         }
-        if (d[i].PName[0]==='道路、隧道災情') {
+        if (d[i].PName==='道路、隧道災情') {
           $scope.total['道路、隧道災情'] += 1;
         }
-        if (d[i].PName[0]==='水利設施災害') {
+        if (d[i].PName==='水利設施災害') {
           $scope.total['水利設施災情'] += 1;
         }
-        if (d[i].PName[0]==='鐵路、高鐵及捷運災情') {
+        if (d[i].PName==='鐵路、高鐵及捷運災情') {
           $scope.total['鐵路、高鐵及捷運災情'] += 1;
         }
-        if (d[i].PName[0]==='車輛及交通事故') {
+        if (d[i].PName==='車輛及交通事故') {
           $scope.total['車輛及交通事故'] += 1;
         }
     
@@ -177,13 +180,19 @@ app.controller('GetDisasterSummary', ['$scope', '$http', function($scope, $http)
       $scope.caseRate = caseRate.toFixed(2);
 
       })
-      
+
 
     .error(function(data, status, headers, config) {
       // log error
       console.log('http error');
     });
 
+    //http://jsfiddle.net/pc7Uu/854/
+    $scope.openInfoWindow = function(e, selectedMarker){
+        //e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+        console.log(selectedMarker);
+    }
 
 }]);
 
